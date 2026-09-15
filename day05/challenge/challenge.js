5fc5a59ded9fe62ea43c9e729686984c482761a3
ilyassaboudou <ilyassaboudou99@gmail.com>
2026-09-15 11:57:25 +0200

    da5 challenge

--- day05/challenge/challenge.js
+++ day05/challenge/challenge.js
@@ -10,7 +10,8 @@
  * Consignes :
  * 1. Vous avez une liste (tableau) de mots interdits : ["noob", "idiot", "nul"].
  * 2. Créez une fonction filtrerMessage(message) qui reçoit le message tapé par le joueur.
- * 3. Si le message contient un des mots interdits (peu importe s'il y a des majuscules, ex: "nOOb"), il doit être remplacé par des astérisques de la même longueur (ex: "").
+ * 3. Si le message contient un des mots interdits (peu importe s'il y a des majuscules, ex: "nOOb"), 
+ * il doit être remplacé par des astérisques de la même longueur (ex: "").
  * 4. Retournez le message censuré.
  *
  * Exemple de test :
@@ -24,3 +25,10 @@
 
 // Découpe d'abord le problème en petites étapes.
 // TODO: écris ta solution ici.
+
+function filtrerMessage(msg) {
+    let interdits = ["noob", "idiot", "nul"]
+    return msg.toLowerCase().split(" ").map(e => interdits.includes(e) ? "*".repeat(e.length) : e).join(" ")
+}
+
+console.log(filtrerMessage("Ce joueur est un véritable noob sur ce jeu vidéo. il est nul et idiot"))
